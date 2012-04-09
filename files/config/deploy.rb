0829c77db240892c43eb26448e688e238c20a3f0
Jason Vasquez <jason.vasquez@developertown.com>
2012-04-09 09:46:53 -0400

    Adding foreman configuration for user-based upstart jobs

--- files/config/deploy.rb
+++ files/config/deploy.rb
@@ -5,11 +5,12 @@ set :rvm_ruby_string, '1.9.3'
 set :rvm_type, :system
 set :rvm_bin_path, '/usr/local/rvm/bin'
 set :deploy_via, :remote_cache
+default_run_options[:pty] = true
 
 # bundler bootstrap
 require 'bundler/capistrano'
 
-# CONFIGURE THIS
+# CONFIGURE THIS - VERY IMPORTANT THAT THERE ARE NO SPACES
 set :application, 'App_Name'
 
 # CONFIGURE THIS
@@ -17,12 +18,12 @@ set :repository,  'ssh://git@github.com/developertown/repo.git'
 set :scm, :git
 
 # CONFIGURE THIS
-set :deploy_to, '/srv/www/app_name'
+set :deploy_to, '/srv/www/appname'
 # CONFIGURE THIS
-set :user, 'app_user'
+set :user, 'appname'
 set :use_sudo, false
 # CONFIGURE THIS
-set :scm_username, 'app_user'
+set :scm_username, 'appname'
 
 # support multiple deployment targets
 set :default_stage, 'ci'
@@ -32,33 +33,40 @@ require 'capistrano/ext/multistage'
 # Rails 3.1+ needs this...
 set :normalize_asset_timestamps, false
 
-before 'deploy:code', :set_tag
+# before 'deploy:code', :set_tag
 after 'deploy:update_code', 'deploy:migrate'
 after 'deploy:update_code', 'deploy:cleanup'
-after "deploy:restart", 'monit:reload'
 
+after 'deploy:update', 'dt:setenv'
+after 'deploy:update', 'foreman:export'
+after 'deploy:update', 'foreman:restart'
 
-before 'monit:start', 'monit:touch'
-before 'monit:reload', 'monit:touch', 'monit:start'
+namespace :dt do
+    desc "Sets the environment file for the app"
+    task :setenv, :roles => :app, :except => {:no_release => true} do
+      run "echo 'RAILS_ENV=#{rails_env}' > #{release_path}/.env"
+      run "echo 'concurrency: web=4' > #{release_path}/.foreman"
+      run "echo 'port: 15000' >> #{release_path}/.foreman"
+    end
+end
 
-namespace :monit do
-  desc "Stop the running instance of monit running as trooptrack"
-  task :stop, :except => {:no_release => true} do
-    run "if ps -p $(cat #{File.join(current_path,'tmp','pids','monit.pid')}) > /dev/null; then kill -9 $(cat #{File.join(current_path,'tmp','pids','monit.pid')}); fi"
+namespace :foreman do
+  desc "Export the Procfile to Ubuntu's upstart scripts"
+  task :export, :roles => :app do
+    run "cd #{release_path} && bundle exec foreman export upstart $HOME/.init -a #{application} -l #{shared_path}/log -t #{release_path}/lib/foreman/templates/upstart"
+  end
+  desc "Start the application services"
+  task :start, :roles => :app do
+    sudo "start #{application}"
   end
 
-  desc "Start the monit daemon running as trooptrack"
-  task :start, :except => {:no_release => true} do
-    run "chmod 700 #{File.join(current_path,'config',"monitrc-#{rails_env}")}; /usr/sbin/monit -c #{File.join(current_path,'config',"monitrc-#{rails_env}")}"
+  desc "Stop the application services"
+  task :stop, :roles => :app do
+    sudo "stop #{application}"
   end
 
-  desc "Reload the configs"
-  task :reload, :except => {:no_release => true} do
-    run "chmod 700 #{File.join(current_path,'config',"monitrc-#{rails_env}")}; /usr/sbin/monit -c #{File.join(current_path,'config',"monitrc-#{rails_env}")} reload"
-  end
-
-  desc "Touch monit config, make sure the permissions are correct"
-  task :touch, :except => {:no_release => true} do
-    run "chmod 700 #{File.join(current_path,'config',"monitrc-#{rails_env}")}"
+  desc "Restart the application services"
+  task :restart, :roles => :app do
+    run "start #{application} || restart #{application}"
   end
 end
